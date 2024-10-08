63153ce26252f0fe88cd01130e9657a94972cd2e
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 <amamrutha04@gmail.com>
2024-10-08 18:34:12 +0530

    test

--- spotter_sale_order_approval/models/sale_order.py
+++ spotter_sale_order_approval/models/sale_order.py
@@ -11,6 +11,30 @@ class SaleOrder(models.Model):
     state = fields.Selection(
         selection_add=[('to_approve', 'To Approve'),('second_approval','Second Approval')]
     )
+    current_user = fields.Many2one('res.users',compute = "_compute_current_user")
+    current_first_approver = fields.Char(compute = "_compute_partner_approval")
+    current_second_approver = fields.Char(compute = "_compute_partner_approval")
+
+    def _compute_current_user(self):
+        self.current_user = self.env.user
+        print(self.current_user,"userrr")
+
+    def _compute_partner_approval(self):
+        self.current_first_approver = self.env['ir.config_parameter'].sudo().get_param('spotter_sale_order_approval.first_user_ids') or False
+        self.current_second_approver = self.env['ir.config_parameter'].sudo().get_param('spotter_sale_order_approval.second_user_ids') or False
+        # self.current_user = self.env.user
+        # for user in first_approver:
+        #     if user == self.current_user.user_id:
+        #         self.current_first_approver = user
+        #         print("crct")
+        #     print(user,"tttt")
+        # print(self.current_user, "userrr")
+        # first = self.current_first_approver
+        # user = self.current_user
+
+        # print("first",first_approver)
+        print("first",self.current_first_approver)
+        print("second",self.current_second_approver)
 
     def action_confirm(self):
         print("hyyy")
@@ -22,7 +46,6 @@ class SaleOrder(models.Model):
 
     def approve_button(self):
         self.state = "second_approval"
-
 
     def second_approval_button(self):
         self.state = "sale"
